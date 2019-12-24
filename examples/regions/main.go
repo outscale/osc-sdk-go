ff5d8cd89a96902be82833c4f3f42edb73a74309
Jérome Jutteau <jerome.jutteau@outscale.com>
2019-12-24 12:25:57 +0100

    examples: add region configuration sample
    - Example how to setup which region to use
    - Print existing subregions (AZ)
    
    Signed-off-by: Jérome Jutteau <jerome.jutteau@outscale.com>

---
+++ examples/regions/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/outscale-dev/osc-sdk-go/osc"
+)
+
+func main() {
+	config := osc.NewConfiguration()
+	config.BasePath, _ = config.ServerUrl(0, map[string]string{"region": "eu-west-2"})
+	client := osc.NewAPIClient(config)
+	auth := context.WithValue(context.Background(), osc.ContextAWSv4, osc.AWSv4{
+		AccessKey: os.Getenv("OSC_ACCESS_KEY"),
+		SecretKey: os.Getenv("OSC_SECRET_KEY"),
+	})
+
+	read, httpRes, err := client.SubregionApi.ReadSubregions(auth, nil)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error while reading subregions")
+		if httpRes != nil {
+			fmt.Fprintln(os.Stderr, httpRes.Status)
+		}
+		os.Exit(1)
+	}
+	println("We have ", len(read.Subregions), "subregions (Availability Zones):")
+	for _, sub := range read.Subregions {
+		println("(", sub.RegionName, ")", sub.SubregionName, ":", sub.State)
+	}
+}
