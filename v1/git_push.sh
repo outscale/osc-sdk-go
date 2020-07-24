38287af7997223f335e45de0789be07b340bb88b
Jérome Jutteau <jerome.jutteau@outscale.com>
2020-07-24 12:35:56 +0200

    remove me
    Signed-off-by: Jérome Jutteau <jerome.jutteau@outscale.com>

--- v1/git_push.sh
+++ v1/git_push.sh
@@ -14,7 +14,7 @@ if [ "$git_host" = "" ]; then
 fi
 
 if [ "$git_user_id" = "" ]; then
-    git_user_id="outscale"
+    git_user_id="jerome-jutteau"
     echo "[INFO] No command line input provided. Set \$git_user_id to $git_user_id"
 fi
 
