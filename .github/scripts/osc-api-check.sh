faf30c31946e15f96da2ce672532bad66833e2a3
Jérôme Jutteau <jerome.jutteau@outscale.com>
2021-01-11 18:23:57 +0100

    ci: periodically check for osc-api change
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

---
+++ .github/scripts/osc-api-check.sh
@@ -0,0 +1,23 @@
+#!/bin/env bash
+set -e
+
+root=$(cd "$(dirname $0)/../.." && pwd)
+github_url="https://api.github.com/repos/outscale/osc-api/releases"
+
+if [ -z "$GITHUB_TOKEN" ]; then
+    echo "GITHUB_TOKEN is missing, abort."
+    exit 1
+fi
+
+osc_api_last_release=$(curl -s -u LolUserName:$GITHUB_TOKEN $github_url | jq ".[] | select(.prerelease == false) | select(.draft == false) | .tag_name" | sort -r | head -n 1 | cut -f 2 -d '"')
+local_api_version=$(cat $root/api_version)
+
+echo "last API version used: $local_api_version"
+echo "last available version: $osc_api_last_release"
+
+if [[ "$local_api_version" = "$osc_api_last_release" ]]; then
+    echo "no update needed, exiting"
+    exit 1
+fi
+
+echo "$osc_api_last_release" > $root/api_version
