8ce9f2723c30fa8ca4d26f02909c1e8884c74a92
Jérôme Jutteau <jerome.jutteau@outscale.com>
2022-03-30 14:50:05 +0200

    config: add config loading classes
    warning: .go files are created from .patch as whole SDK is generated.
    
    closes #352
    
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

---
+++ osc/config_file_test.go
@@ -0,0 +1,179 @@
+package osc
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestBasicConfigFileWithValidEndpoint1(t *testing.T) {
+	ak := os.Getenv("OSC_ACCESS_KEY")
+	sk := os.Getenv("OSC_SECRET_KEY")
+	region := os.Getenv("OSC_REGION")
+	content := fmt.Sprintf(`{
+	"SomeProfile": {
+		"access_key": "%s",
+		"secret_key": "%s",
+		"protocol": "https",
+		"method": "post",
+		"region": "bad-region-that-should-not-impact",
+		"endpoints": {
+			"api": "api.%s.outscale.com/api/v1"
+		}
+	}}`, ak, sk, region)
+	configPath := "/tmp/osc-sdk-go-TestBasicConfigFileWithValidEndpoint1"
+	if err := testDumpToFile(configPath, content); err != nil {
+		t.Fatalf("Error: %s", err.Error())
+	}
+	defer os.Remove(configPath)
+	testBasicConfigWithValidEndpoint(t, configPath)
+}
+
+func TestBasicConfigFileWithValidEndpoint2(t *testing.T) {
+	ak := os.Getenv("OSC_ACCESS_KEY")
+	sk := os.Getenv("OSC_SECRET_KEY")
+	region := os.Getenv("OSC_REGION")
+	content := fmt.Sprintf(`{
+		"SomeProfile": {
+			"access_key": "%s",
+			"secret_key": "%s",
+			"endpoints": {
+				"api": "api.%s.outscale.com/api/v1"
+			}
+		}}`, ak, sk, region)
+	configPath := "/tmp/osc-sdk-go-TestBasicConfigFileWithValidEndpoint2"
+	if err := testDumpToFile(configPath, content); err != nil {
+		t.Fatalf("Error: %s", err.Error())
+	}
+	defer os.Remove(configPath)
+	testBasicConfigWithValidEndpoint(t, configPath)
+}
+
+func testBasicConfigWithValidEndpoint(t *testing.T, configPath string) {
+	configFile, err := LoadConfigFile(&configPath)
+	if err != nil {
+		t.Fatalf("Cannot load configuration file: %s", err.Error())
+	}
+	_, err = configFile.ConfigFromProfileEndpoint("non-existant")
+	if err == nil {
+		t.Fatalf("profile 'non-existant' should not exist")
+	}
+	config, err := configFile.ConfigFromProfileEndpoint("SomeProfile")
+	if err != nil {
+		t.Fatalf("profile 'SomeProfile' should exist (ConfigFromProfileEndpoint()): %s", err.Error())
+	}
+	_, err = configFile.Context("non-existant")
+	if err == nil {
+		t.Errorf("profile 'non-existant' should not exist (Context())")
+	}
+	auth, err := configFile.Context("SomeProfile")
+	if err != nil {
+		t.Fatalf("profile 'SomeProfile' should exist (Context()): %s", err.Error())
+	}
+	client := NewAPIClient(config)
+	_, _, err = client.SubregionApi.ReadSubregions(*auth, nil)
+	if err != nil {
+		t.Fatalf("Error while reading subregions: %s", err.Error())
+	}
+}
+
+func TestBasicConfigFileWithValidRegion1(t *testing.T) {
+	ak := os.Getenv("OSC_ACCESS_KEY")
+	sk := os.Getenv("OSC_SECRET_KEY")
+	region := os.Getenv("OSC_REGION")
+	content := fmt.Sprintf(`{
+		"SomeProfile": {
+			"access_key": "%s",
+			"secret_key": "%s",
+			"protocol": "https",
+			"method": "post",
+			"region": "%s",
+			"endpoints": {
+				"api": "api.bad-region-which-should-not-impact.outscale.com/api/v1"
+			}
+		}}`, ak, sk, region)
+	configPath := "/tmp/osc-sdk-go-TestBasicConfigFileWithValidRegion1"
+	if err := testDumpToFile(configPath, content); err != nil {
+		t.Fatalf("Error: %s", err.Error())
+	}
+	defer os.Remove(configPath)
+	testBasicConfigWithValidRegion(t, configPath)
+}
+
+func TestBasicConfigFileWithValidRegion2(t *testing.T) {
+	ak := os.Getenv("OSC_ACCESS_KEY")
+	sk := os.Getenv("OSC_SECRET_KEY")
+	region := os.Getenv("OSC_REGION")
+	content := fmt.Sprintf(`{
+		"SomeProfile": {
+			"access_key": "%s",
+			"secret_key": "%s",
+			"region": "%s"
+		}}`, ak, sk, region)
+	configPath := "/tmp/osc-sdk-go-TestBasicConfigFileWithValidRegion2"
+	if err := testDumpToFile(configPath, content); err != nil {
+		t.Fatalf("Error: %s", err.Error())
+	}
+	defer os.Remove(configPath)
+	testBasicConfigWithValidRegion(t, configPath)
+}
+
+func testDumpToFile(path string, content string) error {
+	jsonFile, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer jsonFile.Close()
+	if _, err := jsonFile.WriteString(content); err != nil {
+		return err
+	}
+	return nil
+}
+
+func testBasicConfigWithValidRegion(t *testing.T, configPath string) {
+	configFile, err := LoadConfigFile(&configPath)
+	if err != nil {
+		t.Fatalf("Cannot load configuration file: %s", err.Error())
+	}
+	_, err = configFile.ConfigFromProfileEndpoint("non-existant")
+	if err == nil {
+		t.Fatal("profile 'non-existant' should not exist")
+	}
+	config, err := configFile.ConfigFromProfileRegion("SomeProfile")
+	if err != nil {
+		t.Errorf("profile 'SomeProfile' should exist (ConfigFromProfileEndpoint()): %s", err.Error())
+	}
+	_, err = configFile.Context("non-existant")
+	if err == nil {
+		t.Fatal("profile 'non-existant' should not exist (Context())")
+	}
+	auth, err := configFile.Context("SomeProfile")
+	if err != nil {
+		t.Fatalf("profile 'SomeProfile' should exist (Context()): %s", err.Error())
+	}
+	client := NewAPIClient(config)
+	_, _, err = client.SubregionApi.ReadSubregions(*auth, nil)
+	if err != nil {
+		t.Fatalf("Error while reading subregions: %s", err.Error())
+	}
+}
+
+func TestBadConfigFile1(t *testing.T) {
+	content := `{
+		"SomeProfile": {
+			Garbage
+		}}`
+	configPath := "/tmp/osc-sdk-go-TestBadConfigFile2"
+	if err := testDumpToFile(configPath, content); err != nil {
+		t.Fatalf("Error: %s", err.Error())
+	}
+	defer os.Remove(configPath)
+	testBadConfigFile(t, configPath)
+}
+
+func testBadConfigFile(t *testing.T, configPath string) {
+	_, err := LoadConfigFile(&configPath)
+	if err == nil {
+		t.Fatal("Config file should not have loaded")
+	}
+}
