1b6f78267ff384fa0672338be1f04f7935c34f09
Jérôme Jutteau <jerome.jutteau@outscale.com>
2022-04-04 15:05:22 +0200

    tests: check that credentials are set
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

---
+++ .github/scripts/check-creds-settings.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+exit_code=0
+if [ -z "$OSC_ACCESS_KEY" ]; then
+    echo "OSC_ACCESS_KEY var is not set, please fix"
+    exit_code=1
+fi
+if [ -z "$OSC_SECRET_KEY" ]; then
+    echo "OSC_SECRET_KEY var is not set, please fix"
+    exit_code=1
+fi
+if [ -z "$OSC_REGION" ]; then
+    echo "OSC_REGION var is not set, please fix"
+    exit_code=1
+fi
+exit $exit_code
