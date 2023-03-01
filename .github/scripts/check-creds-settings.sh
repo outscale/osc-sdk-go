88b599805c49c6ba624ad1219ca0e3512f2ff928
Jérôme Jutteau <jerome.jutteau@outscale.com>
2023-03-01 11:48:39 +0100

    Use ConfigEnv for Vm example
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

--- .github/scripts/check-creds-settings.sh
+++ .github/scripts/check-creds-settings.sh
@@ -14,4 +14,8 @@ if [ -z "$OSC_REGION" ]; then
     echo "OSC_REGION var is not set, please fix"
     exit_code=1
 fi
+if [ -z "$OMI_ID" ]; then
+    echo "OMI_ID var is not set, please fix"
+    exit_code=1
+fi
 exit $exit_code
