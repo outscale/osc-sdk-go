a4fc26455b9de667e4a7cea3c10bbd5c64dac904
Jean-François Bustarret <jean-francois.bustarret@outscale.com>
2025-12-16 08:57:06 +0000

    ✨ feat(osc): add error helpers

--- pkg/osc/errors.go
+++ pkg/osc/errors.go
@@ -1,6 +1,10 @@
 package osc
 
-import "errors"
+import (
+	"errors"
+	"slices"
+	"strconv"
+)
 
 func (e *ErrorResponse) Error() string {
 	var msg string
@@ -34,4 +38,43 @@ func AsErrorResponse(e error) *ErrorResponse {
 	}
 
 	return nil
+}
+
+func hasErrorCode(err error, codes []string) bool {
+	if err := AsErrorResponse(err); err != nil {
+		for _, e := range err.Errors {
+			if slices.Contains(codes, e.Code) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func hasErrorCodeRange(err error, min, max int) bool {
+	if err := AsErrorResponse(err); err != nil {
+		for _, e := range err.Errors {
+			c, err := strconv.Atoi(e.Code)
+			if err == nil && c >= min && c <= max {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func IsNotFound(err error) bool {
+	return hasErrorCodeRange(err, 5000, 5999)
+}
+
+func IsConflict(err error) bool {
+	return hasErrorCodeRange(err, 6000, 6999) || hasErrorCodeRange(err, 9000, 9999)
+}
+
+func IsAuthError(err error) bool {
+	return hasErrorCode(err, []string{
+		"1", "5", "7", "14", "20", "4120", "4000",
+	})
 }
