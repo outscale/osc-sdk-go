0426cf2891bc7905e7327fa84740a9cc8d2bbd74
Yash Gaykar <yash.gaykar@outscale.com>
2026-03-20 18:19:05 +0530

    :white_check_mark: test: fix existing tests(review comments)

--- examples/project_test.go
+++ examples/project_test.go
@@ -1,7 +1,6 @@
 package examples_test
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
@@ -9,70 +8,55 @@ import (
 	"github.com/outscale/osc-sdk-go/v3/pkg/oks"
 	"github.com/outscale/osc-sdk-go/v3/pkg/options"
 	"github.com/outscale/osc-sdk-go/v3/pkg/profile"
+	"github.com/stretchr/testify/require"
 )
 
 func TestProject(t *testing.T) {
 	userProfile, err := profile.New()
-	if err != nil {
-		panic(err)
-	}
+	require.NoError(t, err)
 
 	client, err := oks.NewClient(userProfile, options.WithLogging(&testingLogger{t}))
-	if err != nil {
-		panic(err)
-	}
+	require.NoError(t, err)
 
 	ctx := t.Context()
 
 	_, err = client.GetProject(ctx, "a-non-existing-project")
-	if err == nil {
-		panic("a-non-existing-project should not exist")
-	}
-	if !oks.IsNotFound(err) {
-		panic(fmt.Errorf("a-non-existing-project should be not found: %w", err))
-	}
+	require.Error(t, err, "a-non-existing-project should not exist")
+	require.True(t, oks.IsNotFound(err), "a-non-existing-project should be not found: %v", err)
 
-	name := "test"
+	name := "osc-sdk-go-test-" + RandomString(10)
 	_, err = client.ListProjects(ctx, &oks.ListProjectsParams{Name: &name})
-	if err != nil {
-		panic(err)
-	}
+	require.NoError(t, err)
 
 	projectTemplate, err := client.GetProjectTemplate(ctx)
-	if err != nil {
-		panic(err)
-	}
+	require.NoError(t, err)
+	require.NotNil(t, projectTemplate.Template)
 
 	project := oks.CreateProjectJSONRequestBody{Name: name}
 	err = mergo.Merge(&project, projectTemplate.Template)
-	if err != nil {
-		panic(err)
-	}
+	require.NoError(t, err)
 
 	var projectID string
 	createProject, err := client.CreateProject(ctx, project)
 	if err != nil {
 		if oks.IsConflict(err) {
 			readtProject, errList := client.ListProjects(ctx, &oks.ListProjectsParams{Name: &name})
-			if errList != nil {
-				panic(err)
-			}
-			if len(readtProject.Projects) != 1 {
-				panic(fmt.Errorf("expected 1 project, got %d", len(readtProject.Projects)))
-			}
+			require.NoError(t, errList)
+			require.Len(t, readtProject.Projects, 1)
 			projectID = readtProject.Projects[0].Id
 		} else {
-			panic(err)
+			require.NoError(t, err)
 		}
 	} else {
+		require.NotNil(t, createProject.Project)
 		projectID = createProject.Project.Id
 	}
+	require.NotEmpty(t, projectID)
 
 	for {
 		readProject, err := client.GetProject(ctx, projectID)
-		if err != nil {
-			panic(err)
-		}
+		require.NoError(t, err)
+		require.NotNil(t, readProject.Project)
 
 		if readProject.Project.Status == oks.ProjectStatusReady {
 			break
@@ -82,7 +66,5 @@ func TestProject(t *testing.T) {
 	}
 
 	_, err = client.DeleteProject(ctx, projectID)
-	if err != nil {
-		panic(err)
-	}
+	require.NoError(t, err)
 }
--- examples/vms_test.go
+++ examples/vms_test.go
@@ -6,23 +6,19 @@ import (
 	"github.com/outscale/osc-sdk-go/v3/pkg/options"
 	"github.com/outscale/osc-sdk-go/v3/pkg/osc"
 	"github.com/outscale/osc-sdk-go/v3/pkg/profile"
+	"github.com/stretchr/testify/require"
 )
 
 func TestReadVms(t *testing.T) {
 	userProfile, err := profile.New()
-	if err != nil {
-		panic(err)
-	}
+	require.NoError(t, err)
 
 	client, err := osc.NewClient(userProfile, options.WithLogging(&testingLogger{t}))
-	if err != nil {
-		panic(err)
-	}
+	require.NoError(t, err)
 
 	read, err := client.ReadVms(t.Context(), osc.ReadVmsRequest{Filters: nil})
-	if err != nil {
-		panic(err)
-	}
+	require.NoError(t, err)
+	require.NotNil(t, read.Vms)
 
 	for i, vm := range *read.Vms {
 		t.Logf("[%d] Id: %s; ImageId: %s", i, vm.VmId, vm.ImageId)
